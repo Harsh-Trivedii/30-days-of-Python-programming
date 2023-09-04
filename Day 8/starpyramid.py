efb48e9e4948903a9ebc138933d44e64b70d9340
Harsh Trivedi <114759277+Harsh-Trivedii@users.noreply.github.com>
2023-09-04 11:58:41 +0530

    Create starpyramid.py

---
+++ Day 8/starpyramid.py
@@ -0,0 +1,13 @@
+# program to print star pattern pyramid
+def pyramid(i,j):
+    while i<=10:
+        j=1
+        while j<=i:
+          print("*",end="")
+          j+=1
+        i+=1
+        print()
+        
+i=1
+j=1
+pyramid(i,j)
